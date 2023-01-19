a23d86d9e0e3bc72c1e09879a36960e6df93ca42
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2023-01-18 20:01:31 -0500

    adjust API

--- pages/api/isValid.ts
+++ pages/api/isValid.ts
@@ -1,35 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Chess } from "chess.js";
+import { ethers } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { goerliProvider } from "services/web3";
+import chessABI from "contracts/chessABI";
 
 type Data = {
   valid: boolean;
   game: string;
-  player: "BLACK" | "WHITE";
   move: string;
+  FEN: string;
+  nextFEN?: string;
+  message?: string;
 };
 type ErrorResponse = {
   error: string;
-  message?: string;
 };
 
 type Response = Data | ErrorResponse;
 interface Body extends NextApiRequest {
-  body: {
+  query: {
     game: string;
-    player: "BLACK" | "WHITE";
     move: string;
   };
 }
 
-export default function handler(req: Body, res: NextApiResponse<Response>) {
-  console.log("GOT REQUEST", req.query);
+export default async function handler(
+  req: Body,
+  res: NextApiResponse<Response>
+) {
+  console.log("RECIEVED REQUEST", req.query);
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
-  const { game, player, move } = req.query;
+  const { game, move } = req.query;
 
-  if (!game || !player || !move) {
+  if (!game || !move) {
     return res.status(405).json({
       error:
         "Did not recieve expected arguments. Recieved:" +
@@ -37,11 +44,35 @@ export default function handler(req: Body, res: NextApiResponse<Response>) {
     });
   }
 
-  // load game from game address
+  // load game
+  let FEN: string;
+  try {
+    const chessContract = new ethers.Contract(game, chessABI, goerliProvider);
+    FEN = await chessContract.FEN();
+    if (!FEN || FEN === "") {
+      return res.status(501).json({ error: "Game not started" });
+    }
+  } catch (e) {
+    console.log("ERROR LOADING FEN", JSON.stringify(e));
+  }
 
-  // game.validateMove
+  // check move
+  const chess = new Chess(FEN!);
 
-  var randomBoolean = Math.random() < 0.5;
+  let response: Response;
+  try {
+    chess.move(move);
+    const nextFEN = chess.fen();
+    response = { valid: true, game, move, nextFEN, FEN: FEN! };
+  } catch (e) {
+    response = {
+      valid: false,
+      FEN: FEN!,
+      game,
+      move,
+      message: "Illegal move",
+    };
+  }
 
-  return res.status(200).json({ valid: randomBoolean, game, player, move });
+  return res.status(200).json(response);
 }
